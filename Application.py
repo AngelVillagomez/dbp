8f9dc0df5cfecd7138838ed68f216cfc916da298
AbeDdelaV <63417788+AbelDdelaV@users.noreply.github.com>
2020-07-29 15:55:19 -0500

    Add files via upload

--- Application.py
+++ Application.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, redirect, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, current_user, login_user, login_required
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route("/home")
 def home():
@@ -15,10 +21,7 @@ def login():
         else:
             return redirect(url_for('home'))
     return render_template('Project.html', error=error)
-#EN PROCESO...
-@app.route("/inlog")
-def inlog():
-    return render_template('index.html')
+
 
 if __name__== '__main__':
     app.run()
